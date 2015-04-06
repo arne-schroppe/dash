1796f9bd5bef7f831f4c6d84fdacea664cb779e4
Arne Schroppe <arne.schroppe@gmx.de>
2015-04-06 03:22:10 +0200

    Better compilation of arguments

--- src/Language/Spot/CodeGen/CodeGen2.hs
+++ src/Language/Spot/CodeGen/CodeGen2.hs
@@ -8,6 +8,8 @@ import Language.Spot.IR.Tac
 
 import Control.Monad.State
 
+import Debug.Trace
+
 compile :: Expr -> ([[Tac Reg]], ConstTable, SymbolNameList)
 compile ast =
         let (result, finalState) = runState (compileExpression ast) emptyCode in
@@ -18,14 +20,16 @@ data CompValue =
     CVResult
   | CVVar String
   | CVTempVar Int
+  deriving (Show, Eq)
 
 data CompilationResult = CompilationResult {
-    code :: [Tac CompValue]
+    instructions :: [Tac CompValue]
   , usedTempVars :: Int
   , freeVariables :: [String]
   }
+  deriving (Show)
 
-emptyResult = CompilationResult { code = [], usedTempVars = 0, freeVariables = [] }
+emptyResult = CompilationResult { instructions = [], usedTempVars = 0, freeVariables = [] }
 
 compileExpression :: Expr -> State Code CompilationResult
 compileExpression expr = case expr of
@@ -41,47 +45,62 @@ compileExpression expr = case expr of
 -}
   a -> error $ "Can't compile: " ++ show a
 
-compileLitNumber n = return $ emptyResult { code = [Tac_load_i CVResult (tw n)] }
+compileLitNumber n = return $ emptyResult { instructions = [Tac_load_i CVResult (tw n)] }
 
 compileLitSymbol name [] = do
   symId <- addSymbolName name
-  return $ emptyResult { code = [Tac_load_ps CVResult symId] }
+  return $ emptyResult { instructions = [Tac_load_ps CVResult symId] }
 compileLitSymbol name args = do
   c <- createConstant $ LitSymbol name args
   cAddr <- addConstant c
-  return $ emptyResult { code = [Tac_load_cs CVResult cAddr] }
+  return $ emptyResult { instructions = [Tac_load_cs CVResult cAddr] }
 
 
-compileFunCall (Var "add") [LitNumber op1, LitNumber op2] = do
-  let code = [ Tac_load_i (CVTempVar 1) (tw op1),
-               Tac_load_i (CVTempVar 2) (tw op2),
-               Tac_add CVResult (CVTempVar 1) (CVTempVar 2)]
-  return $ emptyResult { code = code, usedTempVars = 2 }
-compileFunCall (Var "sub") [LitNumber op1, LitNumber op2] = do
-  let code = [ Tac_load_i (CVTempVar 1) (tw op1),
-               Tac_load_i (CVTempVar 2) (tw op2),
-               Tac_sub CVResult (CVTempVar 1) (CVTempVar 2)]
-  return $ emptyResult { code = code, usedTempVars = 2 }
+compileFunCall (Var "add") [op1, op2] = compileMathFunCall Tac_add op1 op2
+compileFunCall (Var "sub") [op1, op2] = compileMathFunCall Tac_sub op1 op2
+
+compileMathFunCall mf op1 op2 = do
+  (v1, res1) <- compileArgument op1
+  (v2, res2) <- compileArgument op2
+  let instrs2' = increaseTempVars (usedTempVars res1) (instructions res2)
+  let v2' = increaseTempVar (usedTempVars res1) v2
+  let finalInstrs = (instructions res1) ++ instrs2' ++ [mf CVResult v1 v2']
+  let r = emptyResult { instructions = finalInstrs,
+                         usedTempVars = (usedTempVars res1) + (usedTempVars res2) }
+  return $ trace (show r) r
+
+compileArgument (Var v) =
+  return (CVVar v, emptyResult)
+compileArgument e = do
+  exprResult <- compileExpression e
+  let resultTempVar = (usedTempVars exprResult) + 1
+  let instrs' = convertResultToTemp resultTempVar (instructions exprResult)
+  return $ (CVTempVar resultTempVar,
+            exprResult { instructions = instrs', usedTempVars = resultTempVar } )
 
 compileLocalBinding name boundExpr bodyExpr = do
   boundRes <- compileExpression boundExpr
   let bindingTempVar = usedTempVars boundRes + 1
-  let bindingTacs = convertResultToTemp bindingTempVar (code boundRes)
+  let bindingInstrs = convertResultToTemp bindingTempVar (instructions boundRes)
   bodyRes <- compileExpression bodyExpr
-  let bodyTacs = mapTac (incTempVar (bindingTempVar + 1)) (code bodyRes)
-  let bodyTacs1 = mapTac (convVarToTemp name bindingTempVar) bodyTacs
-  return $ emptyResult { code = bindingTacs ++ bodyTacs1 }  -- TODO also return other data
+  let bodyInstrs = increaseTempVars bindingTempVar (instructions boundRes)
+  let bodyInstrs1 = mapTac (convVarToTemp name bindingTempVar) bodyInstrs
+  return $ emptyResult { instructions = bindingInstrs ++ bodyInstrs1 }  -- TODO also return other data
   where
-    incTempVar minTempVar v = case v of
-      CVTempVar n -> CVTempVar $ minTempVar + n
-      a -> a
     convVarToTemp name tempVar v = case v of
       CVVar n | n == name -> CVTempVar tempVar
       a -> a
   
   -- (freeVars, code) <- compileExpression bodyExpr
 
-compileVar a = return $ emptyResult { code = [Tac_move CVResult (CVVar a)] }
+compileVar a = return $ emptyResult { instructions = [Tac_move CVResult (CVVar a)] }
+
+increaseTempVars n tacs =
+  mapTac (increaseTempVar n) tacs
+
+increaseTempVar minTempVar v = case v of
+  CVTempVar n -> CVTempVar $ minTempVar + n
+  a -> a
 
 convertResultToTemp :: Int -> [Tac CompValue] -> [Tac CompValue]
 convertResultToTemp tempVar tacs = 
@@ -110,7 +129,7 @@ createConstant litC =
 
 assignRegisters :: CompilationResult -> [Tac Reg]
 assignRegisters compResult =
-  let codeWithoutRegs = code compResult in
+  let codeWithoutRegs = instructions compResult in
   mapTac assignReg codeWithoutRegs
   where
     assignReg :: CompValue -> Reg
