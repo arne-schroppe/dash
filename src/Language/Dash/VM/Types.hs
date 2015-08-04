f6604da1b63cdff32d92413a875749ee0f87ffbb
Arne Schroppe <arne.schroppe@gmx.de>
2015-08-05 00:05:45 +0200

    Formatting result output

--- src/Language/Dash/VM/Types.hs
+++ src/Language/Dash/VM/Types.hs
@@ -1,6 +1,7 @@
 module Language.Dash.VM.Types where
 
 import Data.Word
+import Data.List (intercalate, intersperse)
 
 type VMWord = Word32
 
@@ -9,7 +10,34 @@ data VMValue =
   | VMSymbol String [VMValue]
   | VMClosure -- TODO add meaningful data 
   | VMFunction -- TODO add meaningful data (name, arguments, etc)
-  deriving (Show, Eq)
+  deriving (Eq)
+
+
+showField :: VMValue -> String
+showField v =
+  case v of
+    s@(VMSymbol _ (_:_)) -> "(" ++ show s ++ ")"
+    _ -> show v
+
+showNestedList :: [VMValue] -> String
+showNestedList vs =
+  "[" ++ (intercalate ", " $ flatValues (VMSymbol "list" vs)) ++ "]"
+  where
+    flatValues (VMSymbol "list" [a, (VMSymbol "empty-list" [])]) = [show a]
+    flatValues (VMSymbol "list" [a, as]) = (show a) : (flatValues as)
+    flatValues x = [show x]
+
+instance Show VMValue where
+  show v =
+    case v of
+      VMNumber i -> show i
+      VMClosure -> "<closure>"
+      VMFunction -> "<function>"
+      VMSymbol "empty-list" [] -> "[]"
+      VMSymbol s [] -> ":" ++ s
+      VMSymbol "list" fields -> showNestedList fields
+      VMSymbol s fields ->  ":" ++ s ++ " " ++ (foldr (++) "" $ intersperse " " $ map showField fields)
+
 
 -- TODO add a show instance that shows this like dash syntax (even with lists and tuples)
 
