1c2f62dad363865eb5964fef69abe9adef9fe4ff
Arne Schroppe <arne.schroppe@gmx.net>
2016-08-01 15:16:53 +0200

    Ideas

--- vm/vm.h
+++ vm/vm.h
@@ -6,6 +6,7 @@
 typedef uint32_t vm_instruction;
 typedef uint32_t vm_value;
 
+// TODO find a way to get errors back into the calling code. also stop on error (i.e. pattern match fail) as long as we don't have exception handling
 vm_value vm_execute(vm_instruction *program, int program_length, vm_value *const_table, int const_table_length);
 vm_value *vm_get_heap_pointer(vm_value addr);
 
