fcfa2aebef36d6e3e966b8a21d14e20f1bd5dac8
Arne Schroppe <arne.schroppe@gmx.net>
2016-08-01 19:46:21 +0200

    Adds multi-line input to repl

--- repl-src/Main.hs
+++ repl-src/Main.hs
@@ -5,20 +5,34 @@ import           System.Console.Haskeline
 import           System.Environment
 import           System.IO
 
+data ReplState = ReplState {
+  rsProg :: Expr,
+  rsMultilineMode :: Bool,
+  rsMultilineInput :: String
+}
+
 main = do
-  putStrLn "Welcome to the Dash repl\nType \"quit\" or \"exit\" to quit"
+  putStrLn "Welcome to the Dash repl\nType \".quit\" to quit.\nUse \"...\" for multiline input (use a blank line to return to single line input)"
   let prog0 = parseWithPreamble ":true" -- obtain preamble
-  runInputT defaultSettings $ loop prog0
-  where
-    loop :: Expr -> InputT IO ()
-    loop prog = do
-      minput <- getInputLine "> "
-      case minput of
-        Nothing -> return ()
-        Just "quit" -> return ()
-        Just "exit" -> return ()
-        Just input -> do prog' <- eval input prog
-                         loop prog'
+  runInputT defaultSettings $ loop $ ReplState prog0 False ""
+
+
+
+loop :: ReplState -> InputT IO ()
+loop state = do
+  minput <- getInputLine $ if rsMultilineMode state then "> " else "λ "
+  case minput of
+    Nothing -> return ()
+    Just ".quit" -> return ()
+    Just ".exit" -> return ()
+    Just "..."   -> loop $ state { rsMultilineMode = True }
+    Just ""      -> do prog' <- eval (rsMultilineInput state) (rsProg state)
+                       loop $ state { rsMultilineMode = False, rsMultilineInput = "", rsProg = prog' }
+    Just input -> do state' <- if rsMultilineMode state
+                               then return $ state { rsMultilineInput = (rsMultilineInput state) ++ "\n" ++ input }
+                               else do prog' <- eval input (rsProg state)
+                                       return $ state { rsProg = prog' }
+                     loop state'
 
 eval :: String -> Expr -> InputT IO Expr
 eval input existingProg = do
